b0768f11ff53d25059567c9845946781edeab91f
aliraza019-js <iamaliraza175@gmail.com>
2022-11-18 13:23:14 +0100

    [TSD-241] Firebase rudimentär eingebunden

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,37 @@
   <div class="text-center text-h3 mt-15">
     Web App Läuft
   </div>
+  <div>
+    Aktuell eingeloggter Benutzer: {{ currentUser }}
+  </div>
 </template>
 
 <script setup lang="ts">
+
+import {onMounted, ref} from "#imports";
+import {useFirebase} from "~/composables/useFirebase";
+import {onAuthStateChanged} from "@firebase/auth";
+
+const currentUser = ref();
+
+onMounted(() => {
+
+  const {app, auth} = useFirebase()
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("user is logged in")
+      console.log("Angemeldeter Benutzer: ", auth.currentUser)
+      currentUser.value = auth.currentUser?.email
+    } else {
+      console.log("user is not logged in")
+      console.log("Angemeldeter Benutzer: ", auth.currentUser)
+      currentUser.value = "N/A"
+    }
+
+
+  })
+})
 
 </script>
 
