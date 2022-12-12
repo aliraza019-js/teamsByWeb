790a39da87747638ac9456c1166ed723f1233dc1
aliraza019-js <iamaliraza175@gmail.com>
2022-12-12 13:50:22 +0100

    TSD-280: added new layout

---
+++ layouts/home.vue
@@ -0,0 +1,95 @@
+<template>
+  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+
+  <Head>
+  </Head>
+  <v-app>
+
+    <v-main>
+      <slot></slot>
+    </v-main>
+
+
+  <v-footer class="footer">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="9" class="align-start">
+          <p class="footer-text order-0">© TeamStage GmbH 2022</p>
+        </v-col>
+        <v-col cols="12" md="3" class="align-end">
+          <v-row>
+            <v-col cols="12" md="3">
+              <div class="d-flex flex-column">
+                <a href="#" class="footer-text order-1">FAQ</a>
+              </div>
+            </v-col>
+            <v-col cols="12" md="3">
+              <div class="d-flex flex-column">
+              <a href="#" class="footer-text order-2">Support</a>
+              </div>
+            </v-col>
+            <v-col cols="12" md="3">
+              <div class="d-flex flex-column">
+              <a href="#" class="footer-text order-3">Contact</a>
+              </div>
+            </v-col>
+            <v-col cols="12" md="3">
+              <div class="d-flex flex-column">
+              <a href="#" class="footer-text order-4">Imprint</a>
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+  </v-app>
+
+  </Html>
+
+</template>
+
+<script setup lang="ts">
+import {
+  ref,
+  useRoute,
+  useSwitchLocalePath,
+  useLocalePath, useI18n, useLocaleHead
+} from '#imports';
+
+const switchLocalePath = useSwitchLocalePath()
+const localePath = useLocalePath();
+const t = useI18n({ useScope: 'global' })
+/**
+ * head configuration
+ */
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: 'id',
+  addSeoAttributes: true
+})
+
+/**
+ * data
+ */
+const drawer = ref(false)
+const route = useRoute()
+
+
+</script>
+
+<style scoped>
+
+.footer {
+  background-color: #f2f2f2;
+  min-height: 48px;
+}
+
+.footer-text {
+  font: normal normal normal 12px/16px Nunito;
+  letter-spacing: 0px;
+  color: #12022F;
+  opacity: 1;
+}
+</style>
+
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,4 +1,5 @@
 <template>
+
   <section>
     <v-container class="my-15 text-left">
       <v-row>
@@ -88,31 +89,6 @@
       </v-row>
     </v-container>
   </section>
-  <v-footer class="footer">
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="10">
-          <p class="footer-text">© TeamStage GmbH 2022</p>
-        </v-col>
-        <v-col cols="12" md="2">
-          <v-row>
-            <v-col cols="12" md="3" class="align-center">
-              <a href="#" class="footer-text">FAQ</a>
-            </v-col>
-            <v-col cols="12" md="3">
-              <a href="#" class="footer-text">Support</a>
-            </v-col>
-            <v-col cols="12" md="3">
-              <a href="#" class="footer-text">Contact</a>
-            </v-col>
-            <v-col cols="12" md="3">
-              <a href="#" class="footer-text">Imprint</a>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-footer>
 </template>
 
 <script setup lang="ts">
@@ -122,29 +98,16 @@ function foo() {
 
 }
 
-
+import {definePageMeta} from "#imports";
+definePageMeta({
+  layout: "home",
+});
 </script>
 
 <style>
 
 @import url('https://fonts.googleapis.com/css?family=Nunito');
 
-.footer {
-  background-color: #f2f2f2;
-  min-height: 48px;
-}
-
-.footer-header {
-  color: #1C1C1C;
-}
-
-.footer-text {
-  text-align: left;
-  font: normal normal normal 12px/16px Nunito;
-  letter-spacing: 0px;
-  color: #12022F;
-  opacity: 1;
-}
 
 .homeCard{
   max-width: 200px ;
