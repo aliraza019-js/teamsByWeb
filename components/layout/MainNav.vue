750662f268bad32bc77208d426a9f72385a30900
aliraza019-js <iamaliraza175@gmail.com>
2022-12-21 12:23:17 +0100

    sep layout 2

--- components/layout/MainNav.vue
+++ components/layout/MainNav.vue
@@ -1,11 +1,34 @@
 <template lang="pug">
-v-layout
-  v-card
+v-layout.nav-card
+  v-card(flat width="100%")
     v-card-text.d-flex.flex-column
-      NuxtLink(to="/de/") Home
-      NuxtLink(to="/de/team") team
-      NuxtLink(to="/de/projects") projects
-      NuxtLink(to="/de/search") search
-      NuxtLink(to="/de/account") account
+      //- nav
+      NuxtLink.d-flex.align-center.justify-start.nav-btn.rounded.pointer(v-for="item in props.navItems" :to="`/${$i18n.locale}${item.to}`")
+        v-icon.mr-2 {{item.icon}}
+        span {{item.titleRef}}
+
       NuxtLink(to="/de/settings") setting
 </template>
+
+<script setup>
+const props = defineProps(['navItems'])
+</script>
+
+<style lang="scss" scoped>
+.nav-card {
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.09);
+  border: 1px solid rgba(226, 226, 226, 0.45);
+  width: 100%;
+}
+
+.nav-btn {
+  height: 60px;
+  width: 100%;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.nav-btn:hover {
+  background-color: #f1f1f1;
+}
+</style>
