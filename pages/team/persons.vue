2b13129777e6d99da2a99554cd8923269b09fead
aliraza019-js <iamaliraza175@gmail.com>
2023-01-30 12:56:09 +0100

    Merged Persons.vue

--- pages/team/persons.vue
+++ pages/team/persons.vue
@@ -1,13 +1,26 @@
 <template lang="pug">
-v-container  
-  v-layout  
-    v-flex
-      CommonPersonList(v-for="person in persons" :person="person" style="float:left; style='display:inline-block'")
-      v-btn(nuxt :to="localePath('/d/team/person/123')") detail page
+v-container
+    v-row
+      v-col(cols="12" sm="4" v-for="(person, index) in persons" :key="index")
+        CommonPersonList(:person="person")
+    v-row.my-5
+      v-divider
+      v-btn.mt-5(nuxt :to="localePath('/d/team/person/123')") detail page
+  
+    v-row.my-5
+      v-divider
+      v-btn.mt-5(color="primary" @click="getPdf()")
+        v-icon.mr-2 mdi-file-pdf
+        span get pdf
 </template>
-
-<script setup>
-const persons = [
+  
+  <script setup>
+  definePageMeta({
+    activeRoute: 'team'
+  })
+  
+  // data
+  const persons = [
   { name: 'Amy Lee', title: 'Senior Project Manager', imgUrl: '../../img/profile_pic_default.png' },
   { name: 'Frank Werner', title: 'Dynamic Optimatization Manager', imgUrl: '../../img/profile_pic_default.png' },
   { name: 'Flo Eutert', title: 'Chief Mobility Strategist', imgUrl: '../../img/profile_pic_default.png' },
@@ -26,8 +39,20 @@ const persons = [
   { name: 'Danny Crane', title: 'Global Communications Supervisor', imgUrl: '../../img/profile_pic_default.png' },
   { name: 'Danny Crane', title: 'Global Communications Supervisor', imgUrl: '../../img/profile_pic_default.png' }
 ]
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  
+  // methods
+  const getPdf = async () => {
+    try {
+      const res = await myFetch('https://intg.team-stage.com/api/users-pdfs')
+      const fileURL = URL.createObjectURL(res)
+      window.open(fileURL)
+    }
+    catch (err) {
+      console.log('err', err)
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
