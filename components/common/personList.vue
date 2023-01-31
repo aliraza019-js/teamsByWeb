8cf6f44deeee968200c84184bfb92d3c2da57146
aliraza019-js <iamaliraza175@gmail.com>
2023-01-31 15:15:26 +0100

    Improvement in responsiveness

--- components/common/personList.vue
+++ components/common/personList.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-v-card(class="" width="100%" height="100%" elevation="3")
-  v-row(align="center" justify="left")
+v-card(class="d-flex" width="100%" height="100%" elevation="3")
+  v-row(style="align-items:center;")
     v-col(cols="auto")
-      v-avatar(size="60")
-        img(class="profilePic" src="./profile_pic_default.png" alt="non found" )
+        v-avatar(class="ml-1" size="56" )
+          img(class="profilePic" src="./profile_pic_default.png" alt="non found" )
     v-col()
       div(class="text-caption") {{ props.person.title }}
       h4 {{props.person.name}}
@@ -17,9 +17,8 @@ console.log('person', props.person)
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css?family=Nunito');
   .profilePic{
-    max-width: 60px;
-    max-height: 60px;  
+    max-width: 56px;
+    max-height: 56px;  
   }
- 
   
 </style>
