a477536a41ab4d9ac0751e1cf996ced004cd8655
aliraza019-js <iamaliraza175@gmail.com>
2023-02-01 20:26:43 +0100

    Refactor teamList

--- components/common/teamList.vue
+++ components/common/teamList.vue
@@ -1,28 +1,33 @@
 <template lang="pug">  
-v-card(class="d-flex" width="100%" height="100%" elevation="3" style="align-items:center;")
-  v-col(style="padding:0px; height:100%; max-width:100px; min-width:53px")   
-      img(class="teamPic" src="./team_pic_default.png" alt="Avatar")
+v-card(class="d-flex rounded-l" width="100%" height="100%" elevation="3" style="align-items:center;")
+  v-col(class="imgCol")   
+      img(:src="team.imgUrl" class="teamPic")
   v-col()
     v-row() 
       body-2 {{ team.title }}
     v-row()  
       h4 {{ team.name  }}
 </template>
-  
-  <script setup>
-  const props = defineProps(['team'])
-  console.log('team', props.team)
-  </script>
-  
-  <style lang="scss" scoped> 
-  
+    
+<script setup>
+const props = defineProps(['team'])
+console.log('team', props.team)
+</script>
+    
+<style lang="scss" scoped> 
+    
   .teamPic{
     width: 90%;
     height: 100%;
     object-fit: cover;
     object-position: center center;
   }
-
-   
-     
-  </style>
+    
+  .imgCol{
+    padding:0px; 
+    height:100%; 
+    max-width:100px; 
+    min-width:53px
+  }
+       
+</style>
