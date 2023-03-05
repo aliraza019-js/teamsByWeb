5a4b631caf533bab63cde7b2bbbc0365c25563d6
aliraza019-js <iamaliraza175@gmail.com>
2023-03-05 08:56:52 +0100

    terms part 2

---
+++ pages/admin.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+v-card(width="100%" flat)
+  v-toolbar(:title="$t('admin.admin')" flat color="transparent")
+    .underline
+    v-btn(icon)
+      v-icon mdi-menu
+
+  v-card-text
+    AdminTerms
+</template>
+
+
+<script setup>
+definePageMeta({
+  activeRoute: 'admin'
+})
+</script>
+
+<style lang="scss" scoped>
+.underline {
+  position: absolute;
+  width: 100%;
+  left: 0px;
+  top: 56px;
+  right: 0px;
+  height: 1px;
+  background-color: rgb(204, 204, 204);
+}
+</style>
