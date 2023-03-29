95818808a1d6b2d205b6b24a1ce7177e2e773e49
aliraza019-js <iamaliraza175@gmail.com>
2023-03-29 11:13:20 +0200

    fix1

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <HomeNavigation/>
+  <HomeNavigation />
 </template>
 
 <script setup lang="ts">
 
 
-import {definePageMeta} from "#imports";
+import { definePageMeta } from "#imports";
 definePageMeta({
-  layout: "home",
+  layout: "default",
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
