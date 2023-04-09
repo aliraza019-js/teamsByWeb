ca74e169bbcdc7e9e950f8d89b6939fa487b46db
aliraza019-js <iamaliraza175@gmail.com>
2023-04-09 21:17:42 +0700

    Update training page

--- pages/account/trainings.vue
+++ pages/account/trainings.vue
@@ -3,29 +3,31 @@ v-container
   div(class="d-flex py-4 justify-end")
     v-btn(variant="text" prepend-icon="mdi-plus" density="compact") Training hinzufügen
   v-row
-    v-col(cols="12")
+    v-col(cols="12" v-for="item, index in certificate" :key="index")
       CommonCard
         template(#title)
           div(class="d-flex flex-column")
-            span(class="text-body-2") 16.05.2019 - 18.05.2019
-            p(class="text-secondary") Scrum Product Owner
+            span(class="text-body-2") {{item.date}}
+            p(class="text-secondary") {{item.certificateTitle}}
         template(#body)
           v-container(class="px-0")
             v-row
-              v-col(cols="6" sm="3")
-
-              v-col(cols="6" sm="9")
-                p Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,.
-                v-row(class="pt-4")
-                  v-col(cols="6" sm="4" class="d-flex align-center")
+              v-col(cols="12" sm="3")
+                v-sheet(border height="170" rounded="lg" class="d-flex align-center justify-center")
+                  div(class="d-flex justify-center align-center")
+                    v-icon(icon="mdi-certificate" color="grey-darken-1" size="90")
+              v-col(cols="12" sm="9")
+                p(class="text-justify") Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,.
+                div(class="d-flex justify-space-between flex-wrap pt-4 align-center")
+                  div(class="d-flex align-center mb-3")
                     v-icon(icon="mdi-domain" color="grey-darken-1")
-                    p(class="pl-2 text-subtitle-2") Some training agency
-                  v-col(cols="6" sm="4" class="d-flex align-center")
+                    p(class="pl-2 text-subtitle-2") {{item.agency}}
+                  div(class="d-flex align-center mb-3")
                     v-icon(icon="mdi-map-marker-radius" color="grey-darken-1")
-                    p(class="pl-2 text-subtitle-2") Bukarest
-                  v-col(cols="6" sm="4" class="d-flex align-center")
+                    p(class="pl-2 text-subtitle-2") {{item.location}}
+                  div(class="d-flex align-center mb-3")
                     v-icon(icon="mdi-certificate" color="grey-darken-1")
-                    p(class="pl-2 text-subtitle-2") Certified Scrum Product Owner
+                    p(class="pl-2 text-subtitle-2") {{item.certificate}}
               v-col(cols="12" class="d-flex justify-space-between")
                 div(class="d-flex align-center")
                   v-icon(style="color: #06A69D" size="large" icon="mdi-message-reply-text")
@@ -40,4 +42,23 @@ v-container
 definePageMeta({
   activeRoute: 'account'
 });
+
+const certificate = ref(
+  [
+    {
+      date: '16.05.2019 - 18.05.2019',
+      certificateTitle: 'Scrum Product Owner',
+      agency: 'Some training agency',
+      location: "Bukarest",
+      certificate: "Certified Scrum Product Owner"
+    },
+    {
+      date: '16.05.2019 - 18.05.2019',
+      certificateTitle: 'NLP Master',
+      agency: 'Some training agency',
+      location: "Hamburg",
+      certificate: "Certified NLP Master"
+    }
+  ]
+);
 </script>
