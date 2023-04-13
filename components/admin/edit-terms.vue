986a366b62962c897b8eae2e3525bebbdf8a15f0
aliraza019-js <iamaliraza175@gmail.com>
2023-04-13 06:23:16 +0200

    terms 2

--- components/admin/edit-terms.vue
+++ components/admin/edit-terms.vue
@@ -3,21 +3,21 @@ ClientOnly
   v-btn(icon variant="text" :size="size || 'small'" @click="editTerm()")
     v-icon {{ props.mode && props.mode == 'edit' ? 'mdi-pencil' : 'mdi-plus'}}
   v-dialog(v-model="dialog" max-width="650px" height="100%")
-    v-card(height="100%")
+    v-card(height="100%" :loading="loading")
 
       v-card-title.text-center 
-        span new term
+        span {{ props.mode == 'edit' ? $t('admin.editTerm') : $t('admin.newTerm') }}
 
       v-card-text
         v-form(ref="form" v-model="valid" lazy-validation)
           v-text-field.streched(
-            label="first form field"
+            :label="t('admin.version')"
             v-model="formData.version"
             :rules="rules.required"
             variant="solo")
 
           .d-flex.align-center.justify-space-between
-            span valid from:
+            span {{ $t('admin.validFrom') }}:
             Datepicker.elevation-3.py-3.my-2.text-center.pointer(
               v-model="formData.validFrom"
               :locale="$i18n.locale == 'de' ? de : enUS"
@@ -25,22 +25,22 @@ ClientOnly
               typeable)
 
           .d-flex.align-center.justify-space-between
-            span valid to:
+            span {{ $t('admin.validUntil') }}:
             Datepicker.elevation-3.py-3.my-2.text-center.pointer(
-              v-model="formData.validTo" 
+              v-model="formData.validUntil" 
               :locale="$i18n.locale == 'de' ? de : enUS"
               :inputFormat="$i18n.locale == 'de' ? 'dd.MM.yyyy' : 'yyyy-MM-dd'"
               typeable)
 
-          .d-flex
-            p some wysiwyg editor
+          p {{ $t('admin.content') }}
 
-          .pa-2.elevation-3.rounded
-            ts-editor(v-model="firstContent")
+          ts-editor.pa-2.elevation-3.rounded(v-model="formData.content")
+
+          v-alert.mt-3(v-if="msgIsVisable" variant="tonal" :type="msgType" closable) {{ msgText }} some msg
 
 
       v-card-actions.d-flex.align-center.justify-center
-        v-btn(variant="text" @click="dialog = false") {{ $t('forms.cancel') }}
+        v-btn(variant="text" @click="closeDialog()") {{ $t('forms.cancel') }}
         v-btn(
           color="secondary" 
           variant="elevated" 
@@ -53,74 +53,81 @@ ClientOnly
 // imports
 import Datepicker from 'vue3-datepicker';
 import { de, enUS } from 'date-fns/locale';
-
-// tiptap
-import { useEditor, EditorContent } from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
-
-const editor = useEditor({
-  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
-  extensions: [StarterKit]
-})
+import { DatetimeFormat } from 'vue-i18n';
 
 // data
-const props = defineProps(['mode', 'size'])
+const props = defineProps(['mode', 'size', 'dataObj'])
 const { t } = useI18n()
+const emit = defineEmits(['updated'])
 
-const firstContent = ref('some first string')
-
-const quillOptions = ref({
-  debug: 'info',
-  contentType: 'html',
-  modules: {
-    toolbar: ['bold', 'italic', 'underline']
-  },
-  theme: 'snow',
-  placeholder: 'edit text here'
-})
-
-const picked = ref(new Date())
-
-// data
+// dialog
 const dialog = ref(null)
+const msgType = ref('error')
+const msgText = ref('')
+const msgIsVisable = ref(false)
+
 // form
 const form = ref(null)
 const valid = ref(false)
 const loading = ref(false)
 const disabled = ref(false)
-const formData = ref({
-  version: '',
-  validFrom: new Date(),
-  validTo: new Date()
-})
+const formData = ref({})
 const rules = reactive({
   required: [v => !!v || t('forms.required')]
 })
-const editorModel = ref('sdsd')
 
 // methods
-const rteUpdated = (content) => {
-  console.log('content from rte', content)
-}
 const editTerm = () => {
   // check if mode is edit and term is provided
   dialog.value = true
-  if (props.mode == 'edit') {
-    console.log('mode is edit')
-  } else {
-    console.log('mode is add')
-  }
+  msgIsVisable.value = false
+  msgText.value = ''
 }
+
+const closeDialog = () => {
+  msgIsVisable.value = false
+  msgText.value = ''
+  dialog.value = false
+}
+
 const validateForm = async () => {
+  loading.value = true
+  disabled.value = true
   const { valid } = await form.value.validate()
-  if (valid) return uploadForm()
+  if (valid) return pushTerm()
 }
-const uploadForm = async () => {
-  console.log('uploading')
+
+const pushTerm = async () => {
+  try {
+    if (props.mode == 'edit') {
+      await myFetch(`/priv/terms/${props.dataObj._id}`, { method: 'PATCH', body: formData.value })
+    } else {
+      await myFetch('/priv/terms', { method: 'POST', body: formData.value })
+    }
+    msgText.value = t('forms.success')
+    msgType.value = 'success'
+  }
+  catch (err) {
+    msgText.value = err.message
+    msgType.value = 'error'
+  }
+  msgIsVisable.value = true
+  loading.value = false
+  disabled.value = false
+  emit('updated')
+  setTimeout(() => {
+    closeDialog()
+  }, 2000)
 }
+
 // hooks
 onMounted(() => {
-  // check if mode == 'edit' => copy provided obj to formData
-  console.log('getting mode', t)
+  msgIsVisable.value = false
+  formData.value.version = props.dataObj?.version || ''
+  formData.value.validFrom = new Date(props.dataObj?.validFrom) || new Date()
+  formData.value.content = props.dataObj?.content || ''
+
+  if (props.dataObj?.validUntil) formData.value.validUntil = props.dataObj.validUntil
+
 })
 </script>
--- pages/admin/terms.vue
+++ pages/admin/terms.vue
@@ -5,16 +5,17 @@ v-card(width="100%" flat)
     template(v-slot:prepend)
       v-btn(icon :to="localePath('/admin')")
         v-icon mdi-arrow-left
-    AdminEditTerms(mode="add" size="default")
+    AdminEditTerms(mode="add" size="default" @updated="updated()")
 
   v-card-text
     v-list
-      v-list-item
-        v-list-item-title Term 1
+      v-list-item(v-for="(item, index) in terms")
+        v-list-item-subtitle.d-flex.align-center
+          v-icon.mr-2(size="x-small") mdi-arrow-right
+          span {{ item.validFrom }}
+        v-list-item-title {{ item.version}}
         template(v-slot:append)
-          AdminEditTerms(mode="edit")
-          //- v-btn(icon size="small" variant="plain")
-            v-icon(size="small") mdi-pencil
+          AdminEditTerms(mode="edit", :data-obj="item" @updated="updated()")
 </template>
 
 <script setup>
@@ -27,7 +28,12 @@ definePageMeta({
 const terms = ref([])
 // methods
 const getTerms = async () => {
-  console.log('trying to get terms')
+  const res = await myFetch('/terms', { method: 'GET' })
+  terms.value = res
+}
+
+const updated = () => {
+  getTerms()
 }
 // hooks
 onMounted(() => {
