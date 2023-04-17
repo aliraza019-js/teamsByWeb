9c53e363f9d144cfe713bf22d072c47d0cfdcd52
aliraza019-js <iamaliraza175@gmail.com>
2023-04-17 08:30:36 +0200

    rte 2

--- components/ts/editor.vue
+++ components/ts/editor.vue
@@ -1,16 +1,34 @@
 <template lang="pug">
 div.flex-flex-column
   bubble-menu(:editor="editor" :tippy-options="{ duration: 100 }" v-if="editor")
+    // Heading
+    v-btn.mr-1(variant="elevated" size="small" :color="editor.isActive('heading') ? 'secondary' : ''" @click="editor.chain().focus().setHeading({Level: 2}).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }")
+      //- v-icon(size="small") mdi-format-h2
+      span.bold H
+    // Format
     v-btn.mr-1(variant="elevated" size="small" :color="editor.isActive('bold') ? 'secondary' : ''" @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }")
       v-icon(size="small") mdi-format-bold
     v-btn.mr-1(variant="elevated" size="small" :color="editor.isActive('italic') ? 'secondary' : ''" @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }")
       v-icon(size="small") mdi-format-italic
+    // Lists
+    v-btn.mr-1(variant="elevated" size="small" :color="editor.isActive('bulletList') ? 'secondary' : ''" @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }")
+      v-icon(size="small") mdi-format-list-bulleted
+    v-btn.mr-1(variant="elevated" size="small" :color="editor.isActive('orderedList') ? 'secondary' : ''" @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }")
+      v-icon(size="small") mdi-numeric
+    // Rules
+    v-btn.mr-1(variant="elevated" size="small" @click="editor.chain().focus().setHorizontalRule().run()")
+      //- v-icon(size="small") mdi-horizontal-rule
+      span.bold ---
 
   editor-content(:editor="editor")
 </template>
 
 <script>
 import StarterKit from '@tiptap/starter-kit'
+// import Document from '@tiptap/extension-document'
+// import Paragraph from '@tiptap/extension-paragraph'
+// import Text from '@tiptap/extension-text'
+// import Heading from '@tiptap/extension-heading'
 import { BubbleMenu, Editor, EditorContent } from '@tiptap/vue-3'
 
 export default {
@@ -51,6 +69,12 @@ export default {
     this.editor = new Editor({
       extensions: [
         StarterKit,
+        // Document,
+        // Paragraph,
+        // Text,
+        // Heading.configure({
+        //   levels: [1, 2, 3],
+        // }),
       ],
       content: this.modelValue,
       onUpdate: () => {
