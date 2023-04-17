ebe3c61cdb57c534e63eb4ef71546d2c198cee09
aliraza019-js <iamaliraza175@gmail.com>
2023-04-18 06:56:15 +0700

    Add project tabs

--- pages/d/project/[id].vue
+++ pages/d/project/[id].vue
@@ -1,9 +1,16 @@
 <template lang="pug">
 v-card(width="100%" flat)
-  v-toolbar(:title="'project detail'" flat color="transparent")
+  v-toolbar(flat color="transparent")
+    v-btn(icon class="ml-0" :to="localePath('project/projects')")
+        v-icon mdi-arrow-left
+    v-toolbar-title Metrohm.com
     .underline
     v-btn(icon)
       v-icon mdi-menu
+    template(v-slot:extension)
+      v-tabs(v-for="tab, index in tabs" :key="index" class="my-5")
+        v-btn(class="tab-btn ma-2" nuxt) {{ $t(tab.label) }}
+
 
   v-card-text
     p.text-body-1 some content
@@ -13,8 +20,22 @@ v-card(width="100%" flat)
 definePageMeta({
   activeRoute: 'project'
 })
-const route = useRoute()
-console.log(route.params.id)
+const route = useRoute();
+
+const tabs = ref(
+  [
+    {
+      label: "projectdetails.general"
+    },
+    {
+      label: "projectdetails.teams"
+    },
+    {
+      label: "projectdetails.comment"
+    }
+  ]
+);
+
 </script>
 
 <style lang="scss" scoped>
@@ -26,5 +47,15 @@ console.log(route.params.id)
   right: 0px;
   height: 1px;
   background-color: rgb(204, 204, 204);
+}
+
+.v-btn.tab-btn {
+  border-radius: 24px;
+  background-color: rgba(28, 28, 28, 0.03);
+}
+
+.v-btn--active.tab-btn {
+  background-image: linear-gradient($secondary, $primary);
+  color: #fff;
 }
 </style>
