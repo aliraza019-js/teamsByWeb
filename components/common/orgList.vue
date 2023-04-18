8e6361c39634f96932f49ac1f7407a9234b99ec1
aliraza019-js <iamaliraza175@gmail.com>
2023-04-18 13:29:43 +0700

    Start General menu id

--- components/common/orgList.vue
+++ components/common/orgList.vue
@@ -12,28 +12,3 @@ v-card(height="65px" rounded="lg")
 <script setup>
 const props = defineProps(['org']);
 </script>
-
-<style lang="scss" scoped>
-
-.place {
-  font: normal normal normal 12px/16px Nunito;
-  color: #1C1C1C;
-  opacity: 1;
-}
-
-.orgName {
-  font: normal normal bold 16px/22px Nunito;
-  letter-spacing: 0px;
-  color: #0966CB;
-  opacity: 1;
-}
-
-.card {
-  min-height: 72px;
-}
-
-.img {
-  max-width:52px;
-  border: 1px solid #E1E1E1;
-}
-</style>
--- pages/d/project/[id].vue
+++ pages/d/project/[id].vue
@@ -13,7 +13,26 @@ v-card(width="100%" flat)
 
 
   v-card-text
-    p.text-body-1 some content
+    v-row
+      v-col(cols="12")
+        CommonCard
+          template(#title)
+            span(class="text-secondary d-flex align-center") Über das Projekt
+          template(#body)
+            v-container(class="px-0")
+              p lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu,Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+      v-col(cols="12")
+        CommonCard
+          template(#title)
+            span(class="text-secondary d-flex align-center") Kunde
+            div(class="d-flex align-center") 
+              img(:src="imgIcon" height="35")
+          template(#body)
+            v-container(class="px-0 pb-0")
+              div(class="d-flex align-start flex-column")
+                div(class="d-flex")
+                  p(class="place") Herisau | Switzerland
+                p(class="orgName") Metrohm Schweiz AG
 </template>
 
 <script setup>
@@ -36,6 +55,8 @@ const tabs = ref(
   ]
 );
 
+
+const imgIcon = ref('https://ik.imagekit.io/teamstage/image_picker_3125430F-511F-43C9-B086-AB64D48351B8-2200-000002773AA5F329_PAgGU5JhU.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1669902515925');
 </script>
 
 <style lang="scss" scoped>
