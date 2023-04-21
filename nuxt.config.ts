8cd59edfde9d42ee26282499ed1ed3496392b783
aliraza019-js <iamaliraza175@gmail.com>
2023-04-21 21:31:17 +0700

    Update and activated mdi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,6 +34,7 @@ export default defineNuxtConfig({
     ],
     css: [
         'vuetify/lib/styles/main.sass',
+        'mdi/css/materialdesignicons.min.css',
         '@/assets/main.scss',
         '@/assets/comps.scss'
     ],
