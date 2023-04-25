cf159e3fb608cb91957a71f6bb0bb35ff41423b7
aliraza019-js <iamaliraza175@gmail.com>
2023-04-25 16:21:01 +0700

    Add nuxt copying

--- nuxt.config copy.ts
+++ nuxt.config copy.ts
@@ -10,18 +10,6 @@ export default defineNuxtConfig({
         FIREBASE_MESSAGING_SENDER_ID: '1074253651684',
         FIREBASE_APP_ID: '1:1074253651684:web:2247589de48557762c96f1',
         FIREBASE_MEASUREMENT_ID: 'G-MBMW935BYG',
-
-        // public: {
-        //     API_URL: process.env.API_URL,
-        //     APP_BASE_URL: process.env.BASE_URL,
-        //     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-        //     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-        //     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-        //     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-        //     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        //     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-        //     FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
-        // }
     },
     app: {
         baseURL: '/app/',
