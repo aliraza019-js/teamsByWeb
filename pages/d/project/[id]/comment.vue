79ae852f5ef3533e966ca1bf2f46b271a2ff3ddb
aliraza019-js <iamaliraza175@gmail.com>
2023-04-27 15:17:38 +0700

    Update comment id

--- pages/d/project/[id]/comment.vue
+++ pages/d/project/[id]/comment.vue
@@ -1,7 +1,16 @@
 <template lang="pug">
 v-row
-  v-col(cols="12" sm="4")
-    span Hello
+  v-col(cols="12")
+    v-card
+      v-card-text(class="d-flex flex-column")
+        div(class="d-flex")
+          span(class="text-caption") 20.06.2021
+        p(class="mt-3 text-justify") Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper tincidunt massa. Suspendisse sapien turpis, hendrerit nec eros vel, luctus tincidunt orci. Cras ac nisi aliquam, sollicitudin turpis et, interdum ante. Aliquam erat volutpat. Proin eget commodo purus, nec pharetra libero. Etiam id ultrices libero, a porta massa. Curabitur vulputate urna ut neque vulputate, eu aliquam turpis sollicitudin. Praesent sollicitudin euismod hendrerit. Phasellus semper tincidunt ligula sit amet lacinia. Morbi consectetur enim vel tortor blandit euismod. Ut sed vestibulum ex.
+        div(class="d-flex mt-3 justify-end")
+          div(class="d-flex gap-10")
+            v-icon(style="color: #06A69D") mdi-thumb-up-outline
+            span(class="font-weight-bold") 1.2K likes
+
 </template>
 
 <script setup>
