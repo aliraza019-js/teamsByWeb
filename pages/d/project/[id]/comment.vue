a957e1e325c0ab8f8b6018061f7f813d4b6f0b7b
aliraza019-js <iamaliraza175@gmail.com>
2023-04-27 15:20:18 +0700

    Update comment [id]

--- pages/d/project/[id]/comment.vue
+++ pages/d/project/[id]/comment.vue
@@ -3,14 +3,14 @@ v-row
   v-col(cols="12")
     v-card
       v-card-text(class="d-flex flex-column")
-        div(class="d-flex")
+        div(class="d-flex align-center justify-space-between")
           span(class="text-caption") 20.06.2021
+          v-avatar(size="30" :image="img")
         p(class="mt-3 text-justify") Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper tincidunt massa. Suspendisse sapien turpis, hendrerit nec eros vel, luctus tincidunt orci. Cras ac nisi aliquam, sollicitudin turpis et, interdum ante. Aliquam erat volutpat. Proin eget commodo purus, nec pharetra libero. Etiam id ultrices libero, a porta massa. Curabitur vulputate urna ut neque vulputate, eu aliquam turpis sollicitudin. Praesent sollicitudin euismod hendrerit. Phasellus semper tincidunt ligula sit amet lacinia. Morbi consectetur enim vel tortor blandit euismod. Ut sed vestibulum ex.
         div(class="d-flex mt-3 justify-end")
           div(class="d-flex gap-10")
             v-icon(style="color: #06A69D") mdi-thumb-up-outline
             span(class="font-weight-bold") 1.2K likes
-
 </template>
 
 <script setup>
@@ -18,6 +18,6 @@ definePageMeta({
   activeRoute: 'project'
 });
 
-
+const img = ref('https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80');
 
 </script>
