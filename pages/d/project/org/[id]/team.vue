4914e5b83c94df18975e73bf6f7457cb6aa14b4c
aliraza019-js <iamaliraza175@gmail.com>
2023-04-28 18:32:15 +0700

    Update project orgs

---
+++ pages/d/project/org/[id]/team.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+div Hello
+</template>
+
+<script setup>
+
+</script>
--- pages/project/orgs.vue
+++ pages/project/orgs.vue
@@ -3,7 +3,7 @@ v-container
   v-row(class="mb-3")
     v-col(cols="12" sm="4" v-for="(org, index) in orgs" :key="index")
       CommonOrgList(:org="org")
-  v-btn(nuxt :to="localePath('/d/project/org/123')" color="blue" variant="outlined")
+  v-btn(nuxt :to="localePath('/d/project/org/123/general')" color="blue" variant="outlined")
     nuxt-icon.mr-2(name="feather/users")
     span detail page
 </template>
